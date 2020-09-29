ef80a6fc5524238175b92233864278f2de33aed4
SergeChurkin <sergechurkin@yahoo.com>
2020-09-28 21:51:43 -0400

    Create excludingNumbers.js
    Output the numbers excluding numbers in exclusion array

---
+++ excludingNumbers.js
@@ -0,0 +1,13 @@
+const excludeNUmbers = [1, 18, 12, 20, 2, 3];
+
+function generateRandom(min, max) {
+    let num = Math.floor(Math.random() * (max - min) + min);
+    return excludeNUmbers.includes(num) ? generateRandom(min, max) : num;
+}
+
+for (let e = 0; e <= 20; e++) {
+    let min = 1;
+    let max = 20;
+    let num = generateRandom(min, max);
+    console.log(num);
+}
